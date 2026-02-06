9bffd5b3b1dd74de53142e4f68ec77afcae0e478
Seoyoung Hong <applesoha@gmail.com>
2026-02-07 02:43:15 +0900

    chore: init react(vite) and setup router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 // App.jsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Home/HomePage.jsx";
+import GamePage from "./pages/Game/GamePage.jsx";
 
 export default function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/game" element={<GamePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
