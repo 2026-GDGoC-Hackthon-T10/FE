cd550b622f9cafffa85d087ffdc9e56de3256fb5
hamsu12 <hamsj3808@gmail.com>
2026-02-06 20:56:13 +0900

    setting

---
+++ src/routes.jsx
@@ -0,0 +1,14 @@
+import HomePage from "./pages/Home/HomePage.jsx";
+import MainPage from "./games/Screen/MainPage.jsx";
+
+const Routes = {
+    HomePage: "HomePage",
+    MainPage: "MainPage",
+
+    screens: {
+        HomePage,
+        MainPage,
+    },
+};
+
+export default Routes;
